4970aaab8b800e8cf97bb3000183084c4a25f1dd
harish <karthickharish@gmail.com>
2021-09-20 07:37:29 -0700

    YP

---
+++ src/NxAuthService/index.js
@@ -0,0 +1,18 @@
+import axios from "axios";
+export const Login = ({}) => {
+    try {
+        const response = await axios({
+            url: conf.baseUrl + 'login',
+            headers : {
+                
+            },
+            method: 'POST',
+            data : data,
+        });
+
+        return {error: false, message: 'Data Sucess!', result: response.data}
+    } catch (error) {
+        return {error: true, message: error.message, result: {}}
+    }
+}
+
